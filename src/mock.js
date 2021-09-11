73a684cd0c63f5b02d04e2cd73ac802d808e6cd7
Jaxongir <jmuxutdinov@mail.ru>
2021-09-11 16:29:12 +0500

    Context-1

---
+++ src/mock.js
@@ -0,0 +1,6 @@
+export const data = [
+    {id:1 ,name:"Jangbogo", url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYyjL3XXFvKTQcyWYvCkqVhyb95dYBrnyfiQ&usqp=CAU"},
+    {id:2 ,name:"Jumong", url:"https://m.media-amazon.com/images/M/MV5BMzlkYWQ0ZDctNmQwNC00MDU2LWFmMDctMjg5OTYyYjhmMmEwXkEyXkFqcGdeQXVyNTcyNTg3Nzc@._V1_FMjpg_UX1000_.jpg"},
+    {id:3 ,name:"Burusli", url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqRyvzuoYvM6uTKGAQbRn1dkq1EhPCtNjgqw&usqp=CAU"},
+    {id:4 ,name:"Terminator", url:"https://upload.wikimedia.org/wikipedia/en/b/b9/Terminator-2-judgement-day.jpg"}
+]
